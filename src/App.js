5017527b57c894a61b140fbae314a5519d5c1786
Nayeem <>
2023-08-28 17:30:24 +0530

    validarion

--- src/App.js
+++ src/App.js
@@ -2,12 +2,41 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-  let weeks=['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday']
-  let weekofday=new Date().getDay()
-  let today=weeks[weekofday]
-  
+  let weeks = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekofday = new Date().getDay();
+  let today = weeks[weekofday];
+
   let [toDos, setTodos] = useState([]);
   let [toDo, setTodo] = useState("");
+  let [message, setMessage] = useState("");
+  console.log(toDos)
+
+  let validate = () => {
+    let s1=toDo.toLowerCase()
+    let s2=toDo.toUpperCase()
+    if (toDos.some(item => item.text === s1 ||item.text === s2 )) {
+      setMessage("Already exists");
+    } else {
+      var letters = /^[A-Za-z]+$/;
+      if(toDo.match(letters)){
+        setTodos([...toDos, { id: Date.now(), text: toDo, status: false }]);
+        setMessage('')
+        setTodo('')
+      }
+      else{
+        setMessage('Please input a valid input')
+      }
+    }
+  };
+
   return (
     <div className="app">
       <div className="mainHeading">
@@ -16,7 +45,6 @@ function App() {
       <div className="subHeading">
         <br />
         <h2>Whoop, it's {today} 🌝 ☕ </h2>
-
       </div>
       <div className="input">
         <input
@@ -26,14 +54,10 @@ function App() {
           placeholder="🖊️ Add item..."
         />
         <i
-          onClick={() =>
-            {
-              setTodos([...toDos, { id: Date.now(), text: toDo, status: false }])
-              setTodo('')
-            }
-           
-            
-          }
+          onClick={() => {
+            validate();
+            setTodo("");
+          }}
           className="fas fa-plus"
         ></i>
       </div>
@@ -45,8 +69,8 @@ function App() {
                 <input
                   type="checkbox"
                   onChange={(e) => {
-                    console.log(e.target.value)
-                    console.log(value)
+                    console.log(e.target.value);
+                    console.log(value);
                     setTodos(
                       toDos.filter((obj) => {
                         if (obj.id == value.id) {
@@ -63,26 +87,40 @@ function App() {
                 <p>{value.text}</p>
               </div>
               <div className="right">
-                <i className="fas fa-times" onClick={()=>{
-                  setTodos(
-                    toDos.filter((obj)=>{
-                      if(obj.id!=value.id){
-                        return obj
-                      }
-                    })
-                  )
-                }}></i>
+                <i
+                  className="fas fa-times"
+                  onClick={() => {
+                    setTodos(
+                      toDos.filter((obj) => {
+                        if (obj.id != value.id) {
+                          return obj;
+                        }
+                      })
+                    );
+                  }}
+                ></i>
               </div>
             </div>
           );
         })}
-        <h4>completed task </h4>
+        <h3 class='completedtasks'>{message}</h3>
+        <div class='completed-tasks'>
+        <h3>completed task </h3>
         {toDos.map((value) => {
           if (value.status) {
-            return (<><ul><li><h5>{value.text}</h5></li></ul></>)
+            return (
+              <>
+                <ul>
+                  <li>
+                    <h4>{value.text}</h4>
+                  </li>
+                </ul>
+              </>
+            );
           }
           return null;
         })}
+        </div>
       </div>
     </div>
   );
